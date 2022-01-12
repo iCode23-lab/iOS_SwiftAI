49b1de43ba7e270eddb5be4d9c84a4d9f5a8e484
Pragatha Muthusamy <69162611+iCode23-lab@users.noreply.github.com>
2022-01-12 07:46:08 -0600

    Add files via upload

---
+++ BarcodeDetection/BarcodeDetection/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BarcodeDetection
+//
+//  Created by Pragatha on 6/8/20.
+//  Copyright © 2020 Pragatha. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ BarcodeDetection/BarcodeDetection/Extensions.swift
@@ -0,0 +1,9 @@
+//
+//  Extensions.swift
+//  BarcodeDetection
+//
+//  Created by Pragatha on 6/8/20.
+//  Copyright © 2020 Pragatha. All rights reserved.
+//
+
+import UIKit
